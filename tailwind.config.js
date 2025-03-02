a2bc0fd450941a38eae507a6b12c3996c63d7503
Md Zahirul Islam <zahirulislam.iv@gmail.com>
2025-03-03 00:17:31 +0600

    but fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./main.js",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary': '#3b82f6',
-      }
-    },
-  },
-  plugins: [],
-}
 
